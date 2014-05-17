053340c340b0470952c79dc459476a1e62716dcf
Russ Kladko <russosx@gmail.com>
2014-05-17 22:04:14 +0300

    Initial Commit

---
+++ osx/ray/ScaryBugsMac/ScaryBugsMac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScaryBugsMac
+//
+//  Created by Russ on 17/05/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ osx/ray/ScaryBugsMac/ScaryBugsMac/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ScaryBugsMac
+//
+//  Created by Russ on 17/05/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
