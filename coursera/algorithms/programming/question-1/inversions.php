7929553b676eb894f67db04039f5e8aec4f4eff3
rooslunn <rooslunn@gmail.com>
2014-05-18 18:34:17 +0300

    Algs week 1 programming task

--- coursera/algorithms/programming/question-1/inversions.php
+++ coursera/algorithms/programming/question-1/inversions.php
@@ -1,8 +1,7 @@
 <?php
 
-
-$filename = "IntegerArrayTest.txt";
-// $filename = "IntegerArray.txt";
+// $filename = "IntegerArrayTest.txt";
+$filename = "IntegerArray.txt";
 
 if ( ($input = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) !== FALSE ) {
 	var_dump(SortAndCount($input));
@@ -10,53 +9,46 @@ if ( ($input = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) !
 	echo "Error opening file\n";
 }
 
-function SortAndCount(array $A) {
-
-	$L = count($A);
-
-	if ( $L  <= 1 )
-		return [$A, 0];
-
-	if ( $L == 2 ) {
-		$i = 0;
-		if ($A[0] > $A[1]) {
-			$i = 1;
-		}
-		sort($A);
-		return [$A, $i];
+function SortAndCount(array $A) 
+{
+	$l = count($A);
+	if ($l < 2) {
+		return 0;
 	}
+	
+	$m = round( (($l + 1) / 2), PHP_ROUND_HALF_DOWN);
+	$left = array_slice($A, 0, $m);
+	$right = array_slice($A, $m, $l);
 
-	list($B, $x) = SortAndCount(LeftArray($A, $L/2));
-	list($C, $y) = SortAndCount(RightArray($A, $L/2));
-	list($D, $z) = MergeAndCountSplit($B, $C);
+	$x = SortAndCount($left);
+	$y = SortAndCount($right);
+	$z = MergeAndCountSplit($A, $left, $right);
 
-	return [$D, $x + $y + $z];
+	return  $x + $y + $z;
 }
 
-function MergeAndCountSplit(array $B, array $C) {
-	$D = [];
-	$z = 0;
-	$i = 0; $j = 0;
-	$N = count($B);
-	for ($k = 0; $k < $N; ++$k) {
-		if ($B[$i] < $C[$j]) {
-			$D[$k] = $B[$i];
-			$i++;
-		} else if ($C[$j] > $B[$i]) {
-			$D[$k] = $C[$j];
+function MergeAndCountSplit(array $A, array $left, array $right) 
+{
+	$i = 0; $j = 0; $count = 0;
+	$NL = count($left);
+	$NR = count($right);
+	while ($i < $NL || $j < $NR) {
+		if ($i == $NL) {
+			$A[$i+$j] = $right[$j];
 			$j++;
-			$z++;
+		} else
+		if ($j == $NR) {
+			$A[$i+$j] = $left[$i];
+			$i++;
+		} else
+		if ($left[$i] < $right[$j]) {
+			$A[$i+$j] = $left[$i];
+			$i++;
+		} else {
+			$A[$i+$j] = $right[$j];
+			$count += $NL - $i;
+			$j++;
 		}
 	}
-	return [$D, $z];
-}
-
-function LeftArray(array $A, $c) {
-	$n = round($c);
-	return array_slice($A, 0, $n);
-}
-
-function RightArray(array $A, $c) {
-	$n = round($c);
-	return array_slice($A, -$n);
+	return $count;
 }
